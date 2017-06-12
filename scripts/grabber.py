60980b9e533f8809f2bcea86004946cd4b87b818
Sophie Saskin <sophie_saskin@brown.edu>
2017-06-12 10:53:52 -0400

    refactored many things.

--- scripts/grabber.py
+++ scripts/grabber.py
@@ -3,17 +3,22 @@ import rospy
 from sensor_msgs.msg import Image
 import cv2
 import cv_bridge
+import time
 
 image_index = 0
-
+time_stamp = 0
 def callback(data):
     global image_index 
+    global time_stamp
     bridge = cv_bridge.CvBridge()
     rospy.loginfo("received an image")
     img = bridge.imgmsg_to_cv2(data)
     #cv2.imshow('image', img)
-    cv2.imwrite('data/{}.png'.format(image_index), img)
-    image_index += 1
+    curr_time = time.time()
+    if(curr_time - time_stamp >= 1):
+        cv2.imwrite('data/{}.png'.format(image_index), img)
+        image_index += 1
+        time_stamp = curr_time
     
 def listener():
 
